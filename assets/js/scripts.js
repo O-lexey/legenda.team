9cf17245dc553439c0d44a6229a48c3dc9daa96f
Aliaksei Zhukau <zhukau.aliaksei@gmail.com>
2018-05-14 11:19:53 +0300

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,105 +1,105 @@
 (function ($) {
-	// Off canvas menu 
-	 $(function ($) {
-		var transformer = $('.js-content'),
-			menuToggle = $('.js-menu-toggle'),
-			menuClose = $('.js-menu__close');
-		 
-		menuToggle.on('click', function (event) {
-			event.preventDefault();
-			transformer.toggleClass('is-open');
-		});
-		
-		menuClose.on('click', function(event) {
-			event.preventDefault();
-			menuToggle.trigger('click');
-		});
-	});
+    // Off canvas menu 
+    $(function ($) {
+        var transformer = $('.js-content'),
+            menuToggle = $('.js-menu-toggle'),
+            menuClose = $('.js-menu__close');
 
-	// EU Cookie Law Bar
-	$(window).on("load", function () {
-		var cookieBar = $('.js-cookie-bar');
+        menuToggle.on('click', function (event) {
+            event.preventDefault();
+            transformer.toggleClass('is-open');
+        });
 
-		if (!cookieBar.length) {
-			return;
-		}
+        menuClose.on('click', function (event) {
+            event.preventDefault();
+            menuToggle.trigger('click');
+        });
+    });
 
-		var cookieBarClose = cookieBar.find('.cookie-bar__close');
+    // EU Cookie Law Bar
+    $(window).on("load", function () {
+        var cookieBar = $('.js-cookie-bar');
 
-		cookieBarClose.on('click', function (event) {
-			event.preventDefault();
-			localStorage.setItem('theme-cookie-bar', true);
-			cookieBar.removeClass('is-sticky');
-		});
+        if (!cookieBar.length) {
+            return;
+        }
 
-		if (!localStorage.getItem('theme-cookie-bar')) {
-			cookieBar.addClass('is-sticky');
-		}
-	});
+        var cookieBarClose = cookieBar.find('.cookie-bar__close');
 
-	// Sticky top bar 
-	$(function ($) {
-		var menu = $('.is-sticky');
-		if (!menu.length) {
-			return;
-		}
+        cookieBarClose.on('click', function (event) {
+            event.preventDefault();
+            localStorage.setItem('theme-cookie-bar', true);
+            cookieBar.removeClass('is-sticky');
+        });
 
-		var previousScroll = $(window).scrollTop();
-		var menuHeight = menu.outerHeight();
-		var menuTop = 0;
+        if (!localStorage.getItem('theme-cookie-bar')) {
+            cookieBar.addClass('is-sticky');
+        }
+    });
 
-		$(window).on('scroll', function () {
-			var currentScroll = $(window).scrollTop();
-			var diff = currentScroll - previousScroll;
-			menuTop -= diff / 2;
+    // Sticky top bar 
+    $(function ($) {
+        var menu = $('.is-sticky');
+        if (!menu.length) {
+            return;
+        }
 
-			if (menuTop < -menuHeight) {
-				menuTop = -menuHeight;
-			}
+        var previousScroll = $(window).scrollTop();
+        var menuHeight = menu.outerHeight();
+        var menuTop = 0;
 
-			if (menuTop > 0) {
-				menuTop = 0;
-			}
+        $(window).on('scroll', function () {
+            var currentScroll = $(window).scrollTop();
+            var diff = currentScroll - previousScroll;
+            menuTop -= diff / 2;
 
-			menu.css('top', menuTop + 'px');
-			previousScroll = currentScroll;
-		});
-	});
-	
-	// Share buttons pop-up
-	$(function () {
-		// link selector and pop-up window size
-		var Config = {
-			Link: ".js-share",
-			Width: 500,
-			Height: 500
-		};
-		// add handler links
-		var slink = document.querySelectorAll(Config.Link);
-		for (var a = 0; a < slink.length; a++) {
-			slink[a].onclick = PopupHandler;
-		}
-		// create popup
-		function PopupHandler(e) {
-			e = (e ? e : window.event);
-			var t = (e.target ? e.target : e.srcElement);
-			// popup position
-			var px = Math.floor(((screen.availWidth || 1024) - Config.Width) / 2),
-				py = Math.floor(((screen.availHeight || 700) - Config.Height) / 2);
-			// open popup
-			var link_href = t.href ? t.href : t.parentNode.href;
-			var popup = window.open(link_href, "social",
-				"width=" + Config.Width + ",height=" + Config.Height +
-				",left=" + px + ",top=" + py +
-				",location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1");
-			if (popup) {
-				popup.focus();
-				if (e.preventDefault) e.preventDefault();
-				e.returnValue = false;
-			}
+            if (menuTop < -menuHeight) {
+                menuTop = -menuHeight;
+            }
 
-			return !!popup;
-		}
-	});
-	
+            if (menuTop > 0) {
+                menuTop = 0;
+            }
+
+            menu.css('top', menuTop + 'px');
+            previousScroll = currentScroll;
+        });
+    });
+   
+    // Share buttons pop-up
+    $(function () {
+        // link selector and pop-up window size
+        var Config = {
+            Link: ".js-share",
+            Width: 500,
+            Height: 500
+        };
+        // add handler links
+        var slink = document.querySelectorAll(Config.Link);
+        for (var a = 0; a < slink.length; a++) {
+            slink[a].onclick = PopupHandler;
+        }
+        // create popup
+        function PopupHandler(e) {
+            e = (e ? e : window.event);
+            var t = (e.target ? e.target : e.srcElement);
+            // popup position
+            var px = Math.floor(((screen.availWidth || 1024) - Config.Width) / 2),
+                py = Math.floor(((screen.availHeight || 700) - Config.Height) / 2);
+            // open popup
+            var link_href = t.href ? t.href : t.parentNode.href;
+            var popup = window.open(link_href, "social",
+                "width=" + Config.Width + ",height=" + Config.Height +
+                ",left=" + px + ",top=" + py +
+                ",location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1");
+            if (popup) {
+                popup.focus();
+                if (e.preventDefault) e.preventDefault();
+                e.returnValue = false;
+            }
+
+            return !!popup;
+        }
+    });
+
 })(jQuery);
